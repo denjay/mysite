f1e70621eb0df67a4b3f6a8e21342bbcf0db937d
denjay <295077487@qq.com>
2017-11-22 13:47:00 +0800

    完成部署

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g9vuvup^xy@gnmmv@(&t)3!%-j*06%qc$j8bs+=2st54q57ml!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT='/var/www/mysite/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
